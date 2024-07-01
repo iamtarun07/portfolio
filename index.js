b2c428f087890a4caa2e630eddd645c4e517d5bc
Tarun Daksh <165006458+iamtarun07@users.noreply.github.com>
2024-07-02 00:39:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const img1=document.querySelector('.image');
+img1.addEventListener('click',function(e){
+    e.preventDefault();
+    window.location.href="https://www.linkedin.com/in/tarun-kumar-a1a684300";
+});
+
+const card1=document.querySelector('.card');
+card1.addEventListener('click',function(e){
+    e.preventDefault();
+    window.location.href="https://github.com/iamtarun07/BlogWebsite";
+});
+
+const card3=document.querySelector('.card3');
+card3.addEventListener('click',function(e){
+    e.preventDefault();
+    window.location.href="https://github.com/iamtarun07/Portfolio-Website";
+});
+
+const card4=document.querySelector('.card4');
+card4.addEventListener('click',function(e){
+    e.preventDefault();
+    window.location.href="https://github.com/iamtarun07/BookMyShow_clone_Project";
+});
